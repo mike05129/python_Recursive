f0fa8cc56adacb84c6aa2f53155205e2c83d22f0
mike05129 <mike05129@gmail.com>
2021-12-29 22:42:44 +0800

    <UPLOAD><ONGONIG> @Still working....
    	modified:   largest_digit.py

--- largest_digit.py
+++ largest_digit.py
@@ -10,20 +10,27 @@ If your implementation is correct, you should see
 
 
 def main():
-	print(find_largest_digit(12345))      # 5
-	print(find_largest_digit(281))        # 8
-	print(find_largest_digit(6))          # 6
-	print(find_largest_digit(-111))       # 1
-	print(find_largest_digit(-9453))      # 9
+    print(find_largest_digit(12345))      # 5
+    print(find_largest_digit(281))        # 8
+    print(find_largest_digit(6))          # 6
+    print(find_largest_digit(-111))       # 1
+    print(find_largest_digit(-9453))      # 9
 
 
 def find_largest_digit(n):
-	"""
-	:param n:
-	:return:
-	"""
-	pass
+    """
+    :param n:
+    :return:
+    """
+
+    dig = n % 10
+    dig2 = (n/10) % 10
+    if dig >= dig2:
+        dig2 = dig
+    if n > 0:
+        find_largest_digit(n/10)
+    return dig2
 
 
 if __name__ == '__main__':
-	main()
+    main()
